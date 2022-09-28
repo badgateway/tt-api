57bec35aaee3f3516acc67b41db938df71c2f02c
syedfkabir <syedfkabir@gmail.com>
2022-09-28 08:28:13 -0400

    Add a12n-server authentication #13

--- src/app.ts
+++ src/app.ts
@@ -16,7 +16,24 @@ const app = new Application();
 // The accesslog middleware shows all requests and responses on the cli.
 app.use(accessLog());
 
+// app.use(accessLog([
+//   '/health',
+//   '/favicon.ico',
+// ]));
+
 app.use(browser());
+
+// app.use(halBrowser({
+//   title: 'Underknown Media API',
+//   navigationLinks: {
+//     export: {
+//       defaultTitle: 'Export',
+//       position: 'header',
+//       showLabel: true,
+//       icon: 'icon/down.svg',
+//     },
+//   }
+// }));
 
 // The problem middleware turns exceptions into application/problem+json error
 // responses.
@@ -36,12 +53,51 @@ app.use(cors({
     'http://127.0.0.1:8902',
   ]
 }));
+// app.use(
+//   cors({
+//     allowHeaders: [
+//       "X-Filename",
+//       "Content-Type",
+//       "User-Agent",
+//       "Authorization",
+//       "Accept",
+//       "Prefer",
+//       "Prefer-Push",
+//       "Link",
+//     ],
+//   })
+// );
 
 // The validator middleware lets users easily validate request bodies
 // using JSON-Schema
 app.use(validator({
   schemaPath: path.join(__dirname, '../node_modules/@badgateway/tt-types/schema')
 }));
+
+// const authApi = process.env.AUTH_API_URI || 'https://auth.underknown.com'; http://localhost:8531
+// const tokenEndpoint = new URL('/token', authApi);
+// const introspectionEndpoint = new URL('/introspect', authApi);
+// const authorizationEndpoint = new URL('/authorize', authApi);
+
+// const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 8505;
+
+// const client = new OAuth2Client({
+//   clientId: process.env.OAUTH2_CLIENT_ID || 'tt-api',
+//   tokenEndpoint: tokenEndpoint.toString(),
+//   introspectionEndpoint: introspectionEndpoint.toString(),
+//   authorizationEndpoint: authorizationEndpoint.toString(),
+// });
+
+// app.use(browserToBearer({client}));
+
+// app.use(oauth2({
+//   client,
+//   publicPrefixes: [
+//     '/health',
+//   ],
+// }));
+
+// app.use(authenticatedAs());
 
 app.use(...routes);
 
--- src/knexfile.ts
+++ src/knexfile.ts
@@ -7,7 +7,7 @@ module.exports = {
       host: '127.0.0.1',
       port: 3306,
       user: 'tt',
-      password: 'password',
+      password: 'Sf173782!',
       database: 'tt',
     },
     migrations: {
