6a848f09d168f57eed6e392dc5f4d49dd330047c
Evert Pot <me@evertpot.com>
2022-10-21 14:28:01 -0400

    Simplify slightly

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,6 @@ import app from './app';
 
 console.log('⏲️  Time tracker v%s', require('../package.json').version);
 
-// The HTTP port can be overridden via the 'PORT' environment variable.
-if (!process.env.PORT) process.env.PORT = '8901';
 app.listen(+process.env.PORT!);
 
 console.log('Running on \x1b[31m%s\x1b[0m', app.origin + '/');
