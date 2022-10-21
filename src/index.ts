33efb6950734f4384e8607bc80582c9a58c4c44b
Evert Pot <me@evertpot.com>
2022-10-21 14:25:20 -0400

    A few inconsequential tweaks
    * Update dependencies.
    * Show the url of the API in the output when the service starts.
    * Show the version of the time tracker API instead of the curveball
      version.
    * Update the lint config to the latest curveball version.

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 /* eslint no-console:0 "@typescript-eslint/no-var-requires": 0 */
 import app from './app';
 
-console.log('⚾ Curveball v%s', require('@curveball/core/package.json').version);
+console.log('⏲️  Time tracker v%s', require('../package.json').version);
 
 // The HTTP port can be overridden via the 'PORT' environment variable.
-const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 8901;
-app.listen(port);
+if (!process.env.PORT) process.env.PORT = '8901';
+app.listen(+process.env.PORT!);
 
-console.log('Listening on port %i', port);
+console.log('Running on \x1b[31m%s\x1b[0m', app.origin + '/');
